79ad9c0addc3e9ebeb83012f8b05af1d5abef173
akirilyuk <a.kirilyuk@cognigy.com>
2022-02-02 10:14:14 +0100

    add first gather node draft

--- src/common/jambonzConfig.ts
+++ src/common/jambonzConfig.ts
@@ -2,62 +2,74 @@ export type BargeInSource = "speech" | "dtmf";
 
 export type SpeechVendor = "azure" | "google" | "aws";
 
+export type SynthesizerConfig = {
+	voice: string;
+	language: string;
+	vendor: SpeechVendor;
+	azure?: {
+		ttsDeploymentId: string;
+	};
+	google?: {
+		disablePunctuation: boolean;
+	};
+};
+
+export type RecognizerConfig = {
+	language?: string;
+	vendor?: SpeechVendor;
+	hints: string[];
+	azure?: {
+		sttDeploymentId?: string; // string containing custom speech model id
+		speechRecognitionMode?: string; // speech recognition mode
+		enableAudioLogging?: boolean;
+		hintsBoost?: number;
+	};
+	google?: {
+		interactionType?: string;
+		hintsBoost?: number;
+	};
+	disableTtsCache?: boolean;
+	continuousASR?: {
+		digits?: string;
+		timeoutInMs?: number;
+	};
+};
+
+export type BargeinConfig = {
+	enable: BargeInSource[];
+	minWordCount?: number;
+};
+
+export type BotOutputConfig = {
+	failOnError?: boolean;
+	noInputGiveUpTimeoutMs?: number;
+	noInputTimeout?: number;
+	noInputRetries?: number;
+	noInputSpeech?: string;
+	noInputUrl?: string;
+};
+
+export type UserInputConfig = {
+	noInputSendEvent?: boolean;
+	noInputTimeout?: number;
+	noInputRetries?: number;
+	noInputSpeech?: string;
+	noInputUrl?: string;
+};
+
+export type DTMFConfig = {
+	interDigitTimeout: number;
+	maxDigits?: number; // what is the default?
+	minDigits?: number; // what is the default
+	submitDigit?: string; // digit we use to triger submitting to bot
+};
 export interface JambonzConfigBase {
-	synthesizer?: {
-		voice: string;
-		language: string;
-		vendor: SpeechVendor;
-		azure?: {
-			ttsDeploymentId: string;
-		};
-		google?: {
-			disablePunctuation: boolean;
-		};
-	};
-	recognizer?: {
-		language?: string;
-		vendor?: SpeechVendor;
-		hints: string[];
-		azure?: {
-			sttDeploymentId?: string; // string containing custom speech model id
-			speechRecognitionMode?: string; // speech recognition mode
-			enableAudioLogging?: boolean;
-		};
-		google?: {
-			interactionType?: string;
-			hintsBoost?: number;
-		};
-		disableTtsCache?: boolean;
-		continuousASR?: {
-			digits?: string;
-			timeoutInMs?: number;
-		};
-	};
-	bargein?: {
-		enable: BargeInSource[];
-		minWordCount?: number;
-	};
-	bot?: {
-		failOnError?: boolean;
-		noInputGiveUpTimeoutMs?: number;
-		noInputTimeout?: number;
-		noInputRetries?: number;
-		noInputSpeec?: string;
-		noInputUrl?: string;
-	};
-	user?: {
-		noInputSendEvent?: boolean;
-		noInputTimeout?: number;
-		noInputRetries?: number;
-		noInputSpeech?: string;
-		noInputUrl?: string;
-	};
-	dtmf?: {
-		interDigitTimeout: number;
-		maxDigits?: number; // what is the default?
-		minDigits?: number; // what is the default
-		submitDigit?: string; // digit we use to triger submitting to bot
-	};
+	synthesizer?: SynthesizerConfig;
+	recognizer?: RecognizerConfig;
+	bargein?: BargeinConfig;
+	bot?: BotOutputConfig;
+	user?: UserInputConfig;
+	dtmf?: DTMFConfig;
 }
 
 export interface JambonzConfig {
--- src/module.ts
+++ src/module.ts
@@ -1,13 +1,13 @@
 import { createExtension } from "@cognigy/extension-tools";
 
 /* import all nodes */
-import { collectUserInput } from "./nodes/collectUserInput";
+import { gather } from "./nodes/gather";
 import { refer } from "./nodes/refer";
 import { hangup } from "./nodes/hangup";
 import { play } from "./nodes/play";
 
 export default createExtension({
-	nodes: [collectUserInput, refer, hangup, play],
+	nodes: [gather, refer, hangup, play],
 
 	connections: []
 });
